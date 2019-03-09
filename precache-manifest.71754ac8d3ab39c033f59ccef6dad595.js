0f696d37e22b9a3baf2a4eead18a7c9fa51696a5
sergioalejandroluna <sergio.alejandro.luna.flores@gmail.com>
2019-03-08 21:03:31 -0700

    Updates

---
+++ precache-manifest.71754ac8d3ab39c033f59ccef6dad595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "098537f6153e03428762",
+    "url": "/resume-app/static/css/main.85b55cfe.chunk.css"
+  },
+  {
+    "revision": "098537f6153e03428762",
+    "url": "/resume-app/static/js/main.098537f6.chunk.js"
+  },
+  {
+    "revision": "6bdff270060655546970",
+    "url": "/resume-app/static/js/runtime~main.6bdff270.js"
+  },
+  {
+    "revision": "f2249a2c1a8bbf511734",
+    "url": "/resume-app/static/js/2.f2249a2c.chunk.js"
+  },
+  {
+    "revision": "e09b3abebea717314fd1c9bba59da155",
+    "url": "/resume-app/index.html"
+  }
+];
